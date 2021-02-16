3a0bfdeb8ac2ae71d6f4015f1afdc119ea1bfd55
Zhaoguo Wang <WangZhaoguo@yahoo.com>
2021-02-16 14:48:58 +0800

    consider line lost and line constraints

--- JuMP.jl
+++ JuMP.jl
@@ -15,8 +15,16 @@ d = 1500;
 # Wind forecast
 w_f = [150, 50];
 
+# Lost rate
+lost_rate = [0.001, 0.001];
+line_num = 2;
+η = ones(line_num) - lost_rate;
+
+# Max power of lines
+f_max = [100, 1000];
+
 # In this cell we create function solve_ed, which solves the economic dispatch problem for a given set of input parameters.
-function solve_ed(g_max, g_min, c_g, c_w, d, w_f)
+function solve_ed(g_max, g_min, c_g, c_w, d, w_f, f_max)
     #Define the economic dispatch (ED) model
     ed = Model(GLPK.Optimizer)
 
@@ -38,8 +46,17 @@ function solve_ed(g_max, g_min, c_g, c_w, d, w_f)
     # Define the constraint on the wind power injection
     @constraint(ed, [i = 1:w_num], w[i] <= w_f[i])
 
+    # Define the constraint on maximum power on each line
+    @variable(ed, bus_out[i = 1:3])
+    @constraint(ed, bus_out[1] == w[1])
+    @constraint(ed, bus_out[2] == bus_out[1] * η[1] + g[1] + w[2])
+    @constraint(ed, bus_out[3] == bus_out[2] * η[2] + g[2])
+    @constraint(ed, [i = 1:2], bus_out[i] <= f_max[i])
+
+
     # Define the power balance constraint
-    @constraint(ed, sum(g) + sum(w) == d)
+    # @constraint(ed, sum(g) + sum(w) == d)
+    @constraint(ed, bus_out[3] == d)
 
     # Solve statement
     optimize!(ed)
@@ -49,7 +66,7 @@ function solve_ed(g_max, g_min, c_g, c_w, d, w_f)
 end
 
 # Solve the economic dispatch problem
-(g_opt, w_opt, ws_opt, obj) = solve_ed(g_max, g_min, c_g, c_w, d, w_f);
+(g_opt, w_opt, ws_opt, obj) = solve_ed(g_max, g_min, c_g, c_w, d, w_f, f_max);
 
 println("\n")
 println("Dispatch of Generators: $(g_opt) MW")
