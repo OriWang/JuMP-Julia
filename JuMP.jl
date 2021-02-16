c3a887723264af0faafca882af95edffab589f97
Zhaoguo Wang <WangZhaoguo@yahoo.com>
2021-02-16 10:58:08 +0800

    allow multiple wind farms

--- JuMP.jl
+++ JuMP.jl
@@ -9,47 +9,51 @@ c_g = [50, 100];
 # Fixed cost of generators
 c_g0 = [1000, 0]
 # Incremental cost of wind generators
-c_w = 50;
+c_w = [50, 50];
 # Total demand
 d = 1500;
 # Wind forecast
-w_f = 200;
+w_f = [150, 50];
 
 # In this cell we create function solve_ed, which solves the economic dispatch problem for a given set of input parameters.
 function solve_ed(g_max, g_min, c_g, c_w, d, w_f)
     #Define the economic dispatch (ED) model
     ed = Model(GLPK.Optimizer)
+
+    # Extract parameters
+    g_num = length(g_max)
+    w_num = length(w_f)
     
     # Define decision variables    
-    @variable(ed, 0 <= g[i = 1:2] <= g_max[i]) # power output of generators
-    @variable(ed, 0 <= w <= w_f) # wind power injection
+    @variable(ed, 0 <= g[i = 1:g_num] <= g_max[i]) # power output of generators
+    @variable(ed, 0 <= w[i = 1:w_num] <= w_f[i]) # wind power injection
 
     # Define the objective function
-    @objective(ed, Min, dot(c_g, g) + c_w * w)
+    @objective(ed, Min, dot(c_g, g) + dot(c_w, w))
 
     # Define the constraint on the maximum and minimum power output of each generator
-    @constraint(ed, [i = 1:2], g[i] <= g_max[i]) #maximum
-    @constraint(ed, [i = 1:2], g[i] >= g_min[i]) #minimum
+    @constraint(ed, [i = 1:g_num], g[i] <= g_max[i]) #maximum
+    @constraint(ed, [i = 1:g_num], g[i] >= g_min[i]) #minimum
 
     # Define the constraint on the wind power injection
-    @constraint(ed, w <= w_f)
+    @constraint(ed, [i = 1:w_num], w[i] <= w_f[i])
 
     # Define the power balance constraint
-    @constraint(ed, sum(g) + w == d)
+    @constraint(ed, sum(g) + sum(w) == d)
 
     # Solve statement
     optimize!(ed)
     
     # return the optimal value of the objective function and its minimizers
-    return value.(g), value(w), w_f - value(w), objective_value(ed)
+    return value.(g), value.(w), w_f - value.(w), objective_value(ed)
 end
 
 # Solve the economic dispatch problem
 (g_opt, w_opt, ws_opt, obj) = solve_ed(g_max, g_min, c_g, c_w, d, w_f);
 
 println("\n")
-println("Dispatch of Generators: ", g_opt, " MW")
-println("Dispatch of Wind: ", w_opt, " MW")
-println("Wind spillage: ", w_f - w_opt, " MW") 
+println("Dispatch of Generators: $(g_opt) MW")
+println("Dispatch of Wind: $(w_opt) MW")
+println("Wind spillage: $(w_f - w_opt) MW") 
 println("\n")
-println("Total cost: ", obj, "\$")
+println("Total cost: $(obj)")
